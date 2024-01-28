fd848da6c1fd4a1e552c798260d74ccb9b9a8071
WilliGee <144535344+WilliGee@users.noreply.github.com>
2024-01-28 20:56:50 +0300

    Completed Assignment

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,20 @@
-def roll_call_dwarves():
+def roll_call_dwarves(dwarves):
+    for i in range(len(dwarves)):
+        print(f"{i + 1}. {dwarves[i]}")    
     pass
 
-def summon_captain_planet():
+def summon_captain_planet(planeteers):
+    return [planeteer.capitalize() + '!' for planeteer in planeteers]
     pass
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(words):
+    for  n in words:
+        if len(n) > 4:
+            return True
+    return False
 
-def find_the_cheese():
+def find_the_cheese(cheese_types):
+    for cheese in cheese_types:
+        if cheese == "gouda" or cheese == "cheddar" or cheese == "camembert":
+            return cheese
     pass
